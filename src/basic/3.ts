599e2c37f0db373aea92882217938e57a894962b
Max-IT-ZT <babuskomax@gmail.com>
2024-08-15 00:51:25 +0300

    first-commit

---
+++ src/basic/3.ts
@@ -0,0 +1,5 @@
+const union: string | number = "Hello"
+console.log('union: ', union);
+const literal: 'enable' | 'disable' = "disable"
+console.log('literal: ', literal);
+
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import "./style.css";
+
+// const counter = <T>(a: T, b: T): T => {
+//   return (a as any) + (b as any);
+// };
+
+// console.log(counter(2, 3));
+// console.log(counter("hello", "world"));
